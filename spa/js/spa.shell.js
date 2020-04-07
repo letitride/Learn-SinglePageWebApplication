c9db6f201594f64c79275867449e30f70b67bb74
letitride <ichikawa.fumiya@gmail.com>
2020-04-07 17:07:55 +0900

    3.5.1 チャットスライダーの拡大や格納を行うメソッドを記述する

--- spa/js/spa.shell.js
+++ spa/js/spa.shell.js
@@ -14,6 +14,11 @@ spa.shell = (function(){
       + '<div class="spa-shell-foot"></div>'
       + '<div class="spa-shell-chat"></div>'
       + '<div class="spa-shell-modal"></div>'
+      ,
+    chat_extend_time: 1000,
+    chat_retract_time: 300,
+    chat_extend_height: 450,
+    chat_retract_height: 15
   },
   stateMap = {$container: null},
   jqueryMap = {},
@@ -21,13 +26,52 @@ spa.shell = (function(){
 
   setJqueryMap = function(){
     var $container = stateMap.$container;
-    jqueryMap = {$container: $container};
+    jqueryMap = {
+      $container: $container,
+      $chat: $container.find(".spa-shell-chat")
+    };
+  };
+
+  //チャットスライダーの開閉処理
+  toggleChat = function(do_extend, callback){
+    var
+      px_chat_ht = jqueryMap.$chat.height(),
+      is_open = px_chat_ht === configMap.chat_extend_height,
+      is_closed = px_chat_ht === configMap.chat_retract_height,
+      is_sliding = !is_open && !is_closed;
+    if(is_sliding){ return false; }
+
+    //チャット欄の拡大
+    if(do_extend){
+      jqueryMap.$chat.animate(
+        {height: configMap.chat_extend_height},
+        configMap.chat_extend_time,
+        function (){
+          if(callback){callback(jqueryMap.$chat);}
+        }
+      );
+      return true;
+    }
+
+    //縮小
+    jqueryMap.$chat.animate(
+      {height: configMap.chat_retract_height},
+      configMap.chat_retract_time,
+      function(){
+        if(callback){callback(jqueryMap.$chat);}
+      }
+    );
+    return true;
   };
 
   initModule = function($container){
     stateMap.$container = $container;
     $container.html(configMap.main_html);
     setJqueryMap();
+
+    //切り替えのテスト
+    setTimeout( function(){ toggleChat(true);}, 3000);
+    setTimeout( function(){ toggleChat(false);}, 8000);
   };
   return {initModule: initModule};
 }());
