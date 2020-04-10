34a6b872d1e8b731363f4d33f80f9ae6d6d7f02f
letitride <ichikawa.fumiya@gmail.com>
2020-04-10 19:56:15 +0900

    7.3.3 ルーティングを別個のNode.jsモジュールに配置する

--- webapp/app.js
+++ webapp/app.js
@@ -3,6 +3,7 @@
 var
   http = require('http'),
   express = require('express'),
+  routes = require('./routes'),
   app = express(),
   server = http.createServer(app);
 
@@ -18,47 +19,14 @@ app.configure( 'development', function(){
   app.use( express.errorHandler({
     dumpExceptions: true,
     showStack: true
-  }) );
+  }));
 });
 
 app.configure( 'production', function(){
   app.use( express.errorHandler() );
 });
 
-app.get( '/', function(request, response){
-  response.redirect( '/spa.html' );
-});
-
-app.all( '/:obj_type/*?', function( request, response, next ){
-  response.contentType( 'json');
-  next();
-});
-
-app.get( '/:obj_type/list', function(request, response){
- response.send({ title: request.params.obj_type + ' list'});
-});
-
-app.post( '/:obj_type/create', function(request, response){
-  response.send( {title: request.params.obj_type + ' created'});
-});
-
-app.get( '/:obj_type/read/:id([0-9]+)', function(request, response){
-  response.send({
-    title: request.params.obj_type + ' with id ' + request.params.id + ' found'
-  });
-});
-
-app.post( '/:obj_type/update/:id([0-9]+)', function(request, response){
-  response.send({
-    title: request.params.obj_type + ' with id ' + request.params.id + ' updated' 
-  });
-});
-
-app.get( '/:obj_type/delete/:id([0-9]+)', function(request, response){
-  response.send({
-    title: request.params.obj_type + ' with id ' + request.params.id + ' deleted'
-  });
-});
+routes.configRoutes( app, server );
 
 server.listen( 3000 );
 
