37c128c9bbce11e8850a2a08e7afe8489643ba84
letitride <ichikawa.fumiya@gmail.com>
2020-04-10 17:13:17 +0900

    Expressで環境を使う
    $ NODE_ENV=production node app.js

--- webapp/app.js
+++ webapp/app.js
@@ -7,10 +7,22 @@ var
   server = http.createServer(app);
 
 app.configure( function(){
-  app.use( express.logger() );
   app.use( express.bodyParser());
   app.use( express.methodOverride() );
 });
+
+app.configure( 'development', function(){
+  app.use( express.logger() );
+  app.use( express.errorHandler({
+    dumpExceptions: true,
+    showStack: true
+  }) );
+});
+
+app.configure( 'production', function(){
+  app.use( express.errorHandler() );
+});
+
 app.get( '/', function(request, response){
   response.send('Hello Express');
 });
